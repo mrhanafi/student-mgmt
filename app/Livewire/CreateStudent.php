f0653b5f548990a259792bae6a711040708de081
unknown <muhdhanafirahman@gmail.com>
2025-06-09 18:53:24 +0800

    form object on create student flow

--- app/Livewire/CreateStudent.php
+++ app/Livewire/CreateStudent.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Livewire\Forms\CreateStudentForm;
 use App\Models\Classes;
 use App\Models\Section;
 use App\Models\Student;
@@ -11,35 +12,40 @@ use Livewire\Component;
 
 class CreateStudent extends Component
 {
-    #[Validate('required',message:"Please provide student name")] 
-    public $name;
-    #[Validate('required|email|unique:students,email')] 
-    public $email;
+    public CreateStudentForm $form;
+
+    // shift to form object
+    // #[Validate('required',message:"Please provide student name")] 
+    // public $name;
+    // #[Validate('required|email|unique:students,email')] 
+    // public $email;
     #[Validate('required')] 
     public $class_id;
-    #[Validate('required')] 
-    public $section_id;
+    // #[Validate('required')] 
+    // public $section_id;
 
-    public $sections = [];
+    // public $sections = [];
 
     public function addStudent()
     {
         // dd($this->section_id);
-        $this->validate();
+        // $this->validate();
         
-        Student::create([
-            'name' => $this->name,
-            'email' => $this->email,
-            'class_id' => $this->class_id,
-            'section_id' => $this->section_id,
-        ]);
+        // Student::create([
+        //     'name' => $this->name,
+        //     'email' => $this->email,
+        //     'class_id' => $this->class_id,
+        //     'section_id' => $this->section_id,
+        // ]);
+        $this->form->storeStudent($this->class_id);
 
         return redirect(route('students.index'));
     }
 
     public function updatedClassId($value)
     {
-        $this->sections = Section::where('class_id',$value)->get();
+        // $this->sections = Section::where('class_id',$value)->get();
+        $this->form->setSections($value);
     }
 
     // #[Layout('layouts.app')]
