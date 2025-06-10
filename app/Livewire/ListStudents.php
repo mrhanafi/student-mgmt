af709e1c7575cb3ab2cabd5e12fc0528dcde2ba6
unknown <muhdhanafirahman@gmail.com>
2025-06-10 15:29:43 +0800

    search filter

--- app/Livewire/ListStudents.php
+++ app/Livewire/ListStudents.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Student;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -10,13 +11,28 @@ use Livewire\WithPagination;
 class ListStudents extends Component
 {
     use WithPagination;
+    
+    public string $search = '';
 
     // #[Layout('layouts.app')] dah generate livewire config file, xyah pakai da line ni
     public function render()
     {
+        $query = Student::query();
+
+        $query = $this->applySearch($query);
+
         return view('livewire.list-students',[
-            'students' => Student::orderBy('id','DESC')->paginate()
+            'students' => $query->orderBy('id','DESC')->paginate(10)
         ]);
+    }
+
+    public function applySearch(Builder $query)
+    {
+        return $query->where('name','like','%'.$this->search.'%')
+        ->orWhere('email','like','%'.$this->search.'%')
+        ->orWhereHas('class',function($query){
+            $query->where('name','like','%'.$this->search.'%');
+        });
     }
 
     public function deleteStudent($studentId)
